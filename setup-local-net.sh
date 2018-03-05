7ffca0823e010f4373bec055a3b5741facf5db14
Andrew Gilbert <andrewg800@gmail.com>
2018-03-05 09:22:58 -0800

    Network config
    Also tweaks to scripts, and commands to set up NAT on Linux.
    
    diff --git a/interfaces b/interfaces
    new file mode 100644
    index 0000000..ae2d9b5
    --- /dev/null
    +++ b/interfaces
    @@ -0,0 +1,14 @@
    +# interfaces(5) file used by ifup(8) and ifdown(8)
    +
    +# Please note that this file is written to be used with dhcpcd
    +# For static IP, consult /etc/dhcpcd.conf and 'man dhcpcd.conf'
    +
    +# Include files from /etc/network/interfaces.d:
    +source-directory /etc/network/interfaces.d
    +
    +auto io
    +iface io inet loopback
    +
    +auto eth0
    +iface eth0 inet dhcp
    +	dns-nameservers 8.8.8.8 8.8.4.4
    diff --git a/playbook.yml b/playbook.yml
    index 8cf5f00..fcb8ecf 100644
    --- a/playbook.yml
    +++ b/playbook.yml
    @@ -1,4 +1,5 @@
     - hosts: main
    +  strategy: free
       tasks:
         - name: Install decoder service description file
           become: true
    diff --git a/setup-local-net.sh b/setup-local-net.sh
    new file mode 100755
    index 0000000..6f3e79d
    --- /dev/null
    +++ b/setup-local-net.sh
    @@ -0,0 +1,4 @@
    +ip addr add dev enp0s25 192.168.1.0/24
    +iptables -A POSTROUTING -o wlp3s0 -j MASQUERADE
    +iptables -A FORWARD -s 192.168.1.0/24 -i enp0s25 -o wlp3s0 -m conntrack --ctstate NEW -j ACCEPT
    +iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
    diff --git a/setup.yml b/setup.yml
    index 8946ee7..ceece18 100644
    --- a/setup.yml
    +++ b/setup.yml
    @@ -10,12 +10,11 @@
             name: ada
         - name: Add avahi-daemon
           apt:
    +        update_cache: yes
             name: avahi-daemon
    -        state: present
         - name: Install docker
           apt:
             name: docker.io
    -        state: present
         - name: Add docker group
           group:
             name: docker
    @@ -38,3 +37,7 @@
             password: $6$viole3wVgD6Wlz$FHn3xscVuWuibRzbHhbu7B4N8.dn9Hox4nTgvprqWT9RsB7e2mSRDWusS.MzFc4TrHW3sPep9Jh561CU6qocr.
         - name: Lock user account
           command: passwd -l pi
    +    - name: Copy DNS config
    +      copy:
    +        src: interfaces
    +        dest: /etc/network/interfaces

---
+++ setup-local-net.sh
@@ -0,0 +1,4 @@
+ip addr add dev enp0s25 192.168.1.0/24
+iptables -A POSTROUTING -o wlp3s0 -j MASQUERADE
+iptables -A FORWARD -s 192.168.1.0/24 -i enp0s25 -o wlp3s0 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
