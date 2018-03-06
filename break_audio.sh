79e9ec934a37015a65b4e288d79c5ceeac2eaf6d
Andrew Gilbert <andrewg800@gmail.com>
2018-03-06 14:01:06 -0800

    Use new directory for break_audio

--- break_audio.sh
+++ break_audio.sh
@@ -1,1 +1,1 @@
-sox englandandyesterday_01_guiney.ogg -c1 -r16000 sentences.wav silence 1 0.1 1% 1 10 .1% pad 1.5 1.5 : newfile : restart
+sox englandandyesterday_01_guiney.ogg -c1 -r16000 input/sentences.wav silence 1 0.1 1% 1 10 .1% pad 1.5 1.5 : newfile : restart
